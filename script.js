e2069bc9d13d194118f25ddc21c35434d3ab712b
richterndzh <richterndzh@gmail.com>
2022-01-31 20:01:54 +0300

    Задание 3

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
-const num = 266219;
+//Через if
+let lang = prompt("Введите значние 'ru' или 'en'");
+if (lang == 'ru') {
+  let arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+  alert(arr);
+} else if (lang == 'en') {
+  let arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+  alert(arr);
+}
 
-const multiplyingDigitsNumber = str => {
-    let res = 1;
-    for (let i = 0; i < str.length; i++) {
-        res *= str[i];
-    }
-    return res;
+// Через switch
+let lang1 = prompt("Введите значние 'ru' или 'en'");
+switch (lang1){
+  case 'ru':
+    let arr1 = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+    alert(arr1);
+  break;
+  case 'en':
+    let arr01 = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+    alert(arr01);
+  break;
+}
+
+//Через многомерный массив
+let lang2 =prompt("Введите значние 'ru' или 'en'");
+let arr2 = {
+  'ru':['пн', 'вт', 'ср','чт', 'пт', 'сб', 'вс'],
+	'en':['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
 };
+alert(arr2[lang2]);
 
-const multiplyingDigits = multiplyingDigitsNumber(num.toString());
-console.log('2: ', multiplyingDigits);
+//Задача вторая с тернарными операторами
+const namePerson = prompt('Введите имя');
 
-const raising = multiplyingDigits ** 3;
-console.log('3: ', raising);
-
-console.log('4: ', raising.toString().substr(0,2));
+let result = namePerson === "Артем" ? "Директор" : namePerson === "Александр" ? "Преподаватель" : "Студент";
+//let result = namePerson === "Александр" ? "Преподаватель" : "Данных нет";
+//let result = namePerson === namePerson ? "Студент" : "Данных нет";
+console.log(result);
+// console.log(result1);
+// console.log(result2);
